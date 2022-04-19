9272c4e7295a32c50853cf4493ae45662323b946
Mike Powers <72277454+Aphelious@users.noreply.github.com>
2022-04-19 09:56:20 -0400

    Add clean_scooter()

--- escooter_dag.py
+++ escooter_dag.py
@@ -12,3 +12,11 @@ default_args = {
     'retry_delay': dt.timedelta(minutes=5)
 }
 
+def clean_scooter():
+    scooter_csv = r'/Users/mike/Desktop/Main/Programming/Projects/Tutorials/dataeng/CleaningData/escooter/scooter.csv'
+    df = pd.read_csv(scooter_csv)   # Read in csv file
+    df.drop(columns=['region_id'], inplace=True)    # Drop the region_id column
+    df.columns = [x.lower() for x in df.columns]    # Reformat the column names to lowercase
+    df['started_at'] = pd.to_datetime(df['started_at'], format= '%m/%d/%y %H:%M')   # Cast generic object to dt object
+    df.to_csv(r'/Users/mike/Desktop/Main/Programming/Projects/Tutorials/dataeng/CleaningData/escooter/clean_scooter.csv')
+
